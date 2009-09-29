df5a89d62d0cb75cb718773c19f29faecb48e679
jeffgenovy@gmail.com <jeffgenovy@gmail.com@292b94a2-9e6e-11de-8512-85e150c422c3>
2009-09-29 21:19:39 +0000

    Adding support for using SQLite as a database backend.

--- radmode_database.sql
+++ radmode_database.sql
@@ -14,11 +14,10 @@ USE `urt_rad`;
 -- Table `urt_rad`.`weapons`
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `urt_rad`.`weapons` (
-  `weapon_id` TINYINT UNSIGNED NOT NULL ,
+  `weapon_id` TINYINT UNSIGNED PRIMARY KEY ,
   `internal_name` VARCHAR(45) NOT NULL ,
   `weapon_name` VARCHAR(64) NOT NULL ,
-  `kills` BIGINT UNSIGNED NOT NULL ,
-  PRIMARY KEY (`weapon_id`) )
+  `kills` BIGINT UNSIGNED NOT NULL )
 ENGINE = MyISAM  ROW_FORMAT = FIXED;
 
 
@@ -26,14 +25,13 @@ ENGINE = MyISAM  ROW_FORMAT = FIXED;
 -- Table `urt_rad`.`gametypes`
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `urt_rad`.`gametypes` (
-  `game_id` TINYINT UNSIGNED NOT NULL ,
+  `game_id` TINYINT UNSIGNED PRIMARY KEY ,
   `game_name` VARCHAR(45) NOT NULL ,
   `total_rounds` BIGINT UNSIGNED NOT NULL ,
   `total_time` BIGINT UNSIGNED NOT NULL ,
   `longest_round` BIGINT UNSIGNED NOT NULL ,
   `shortest_round` BIGINT UNSIGNED NOT NULL ,
-  `avg_round` BIGINT UNSIGNED NOT NULL ,
-  PRIMARY KEY (`game_id`) )
+  `avg_round` BIGINT UNSIGNED NOT NULL )
 ENGINE = MyISAM  ROW_FORMAT = FIXED;
 
 
@@ -41,11 +39,10 @@ ENGINE = MyISAM  ROW_FORMAT = FIXED;
 -- Table `urt_rad`.`maps`
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `urt_rad`.`maps` (
-  `map_id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
+  `map_id` INTEGER UNSIGNED PRIMARY KEY ,
   `map_name` VARCHAR(144) NOT NULL ,
-  `times_played` INT UNSIGNED NULL ,
-  `duration` BIGINT UNSIGNED NOT NULL DEFAULT 0 ,
-  PRIMARY KEY (`map_id`) )
+  `times_played` INTEGER UNSIGNED NULL ,
+  `duration` BIGINT UNSIGNED NOT NULL DEFAULT 0 )
 ENGINE = MyISAM  ROW_FORMAT = FIXED;
 
 
@@ -53,14 +50,13 @@ ENGINE = MyISAM  ROW_FORMAT = FIXED;
 -- Table `urt_rad`.`current_players`
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `urt_rad`.`current_players` (
-  `slot_num` TINYINT UNSIGNED NOT NULL ,
+  `slot_num` TINYINT UNSIGNED PRIMARY KEY ,
   `score` SMALLINT NOT NULL ,
   `ping` SMALLINT UNSIGNED NOT NULL ,
   `name` CHAR(32) NOT NULL ,
-  `ip` INT UNSIGNED NOT NULL ,
+  `ip` INTEGER UNSIGNED NOT NULL ,
   `qport` SMALLINT UNSIGNED NOT NULL ,
-  `rate` SMALLINT UNSIGNED NOT NULL ,
-  PRIMARY KEY (`slot_num`) )
+  `rate` SMALLINT UNSIGNED NOT NULL )
 ENGINE = MyISAM  ROW_FORMAT = FIXED;
 
 
@@ -72,7 +68,7 @@ CREATE  TABLE IF NOT EXISTS `urt_rad`.`status` (
   `client_request` TINYINT UNSIGNED NULL DEFAULT 0 ,
   `log_lines_processed` BIGINT UNSIGNED NULL DEFAULT 0 ,
   `log_bytes_processed` BIGINT UNSIGNED NULL DEFAULT 0 ,
-  `log_last_check` INT UNSIGNED NULL DEFAULT 0 ,
+  `log_last_check` INTEGER UNSIGNED NULL DEFAULT 0 ,
   `last_update` DATETIME NULL )
 ENGINE = MyISAM  ROW_FORMAT = FIXED;
 
@@ -81,11 +77,10 @@ ENGINE = MyISAM  ROW_FORMAT = FIXED;
 -- Table `urt_rad`.`players`
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `urt_rad`.`players` (
-  `player_id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
+  `player_id` INTEGER UNSIGNED PRIMARY KEY ,
   `name` VARCHAR(32) NOT NULL ,
   `duration` BIGINT UNSIGNED NOT NULL DEFAULT 0 ,
-  `creation` DATETIME NULL ,
-  PRIMARY KEY (`player_id`) )
+  `creation` DATETIME NULL )
 ENGINE = MyISAM  ROW_FORMAT = FIXED;
 
 
@@ -93,9 +88,9 @@ ENGINE = MyISAM  ROW_FORMAT = FIXED;
 -- Table `urt_rad`.`ips`
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `urt_rad`.`ips` (
-  `ip` INT UNSIGNED NOT NULL ,
+  `ip` INTEGER UNSIGNED NOT NULL ,
   `ip_text` CHAR(15) ASCII NOT NULL ,
-  `player_id` INT UNSIGNED NULL ,
+  `player_id` INTEGER UNSIGNED NULL ,
   `creation` DATETIME NULL ,
   INDEX `ip` (`ip` ASC) ,
   INDEX `player_id` (`player_id` ASC) ,
@@ -111,10 +106,9 @@ ENGINE = MyISAM  ROW_FORMAT = FIXED;
 -- Table `urt_rad`.`guids`
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `urt_rad`.`guids` (
-  `guid_id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
+  `guid_id` INTEGER UNSIGNED PRIMARY KEY ,
   `guid` CHAR(32) ASCII NOT NULL ,
-  `player_id` INT UNSIGNED NULL ,
-  PRIMARY KEY (`guid_id`) ,
+  `player_id` INTEGER UNSIGNED NULL ,
   INDEX `player_id` (`player_id` ASC) ,
   CONSTRAINT `player_id`
     FOREIGN KEY (`player_id` )
@@ -128,13 +122,12 @@ ENGINE = MyISAM  ROW_FORMAT = FIXED;
 -- Table `urt_rad`.`rcon_log`
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `urt_rad`.`rcon_log` (
-  `log_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT ,
+  `log_id` BIGINT UNSIGNED PRIMARY KEY ,
   `datetime` DATETIME NOT NULL ,
-  `player_id` INT UNSIGNED NOT NULL ,
-  `ip` INT UNSIGNED NULL ,
+  `player_id` INTEGER UNSIGNED NOT NULL ,
+  `ip` INTEGER UNSIGNED NULL ,
   `slot` TINYINT UNSIGNED NULL ,
   `action` TINYINT UNSIGNED NULL ,
-  PRIMARY KEY (`log_id`) ,
   INDEX `player_id` (`player_id` ASC) ,
   INDEX `ip` (`ip` ASC) ,
   CONSTRAINT `player_id`
@@ -154,19 +147,18 @@ ENGINE = MyISAM  ROW_FORMAT = FIXED;
 -- Table `urt_rad`.`servers`
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `urt_rad`.`servers` (
-  `server_id` TINYINT UNSIGNED NOT NULL AUTO_INCREMENT ,
+  `server_id` TINYINT UNSIGNED PRIMARY KEY ,
   `status` TINYINT NULL DEFAULT 0 ,
-  `ip` INT UNSIGNED NULL ,
+  `ip` INTEGER UNSIGNED NULL ,
   `port` MEDIUMINT UNSIGNED NULL DEFAULT 27960 ,
   `name` VARCHAR(64) NULL ,
   `current_map` VARCHAR(45) NULL ,
-  `timeouts` INT UNSIGNED NULL DEFAULT 0 ,
-  `timeout_last` INT UNSIGNED NULL ,
-  `timeout_delay` INT UNSIGNED NULL DEFAULT 5 ,
-  `timeout_wait_delay` INT UNSIGNED NULL DEFAULT 10 ,
+  `timeouts` INTEGER UNSIGNED NULL DEFAULT 0 ,
+  `timeout_last` DATETIME NULL ,
+  `timeout_delay` INTEGER UNSIGNED NULL DEFAULT 5 ,
+  `timeout_wait_delay` INTEGER UNSIGNED NULL DEFAULT 10 ,
   `rcon_pw` VARCHAR(32) NULL ,
-  `svars` TEXT(1024) NULL ,
-  PRIMARY KEY (`server_id`) )
+  `svars` TEXT(1024) NULL )
 ENGINE = MyISAM;
 
 
